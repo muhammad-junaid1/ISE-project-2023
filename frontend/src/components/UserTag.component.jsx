5d07338c79a17676f629555eb1e78acb62ef7dab
Muhammad Junaid <mjunaid.swe@gmail.com>
2023-05-06 11:46:09 -0700

    ui updated

--- frontend/src/components/UserTag.component.jsx
+++ frontend/src/components/UserTag.component.jsx
@@ -3,7 +3,7 @@ import "../styles/user-tag.css";
 const UserTag = ({type}) => {
     return (
         <>
-            <div className="user-tag">
+            <div className={`user-tag post_${type.toLowerCase()}`}>
                 <p>{type}</p>
             </div>
         </>
