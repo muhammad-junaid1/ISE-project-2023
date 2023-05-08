d8e14c9f9d065acd25d87838abb228fd01b8006a
Muhammad Junaid <mjunaid.swe@gmail.com>
2023-05-08 13:23:33 -0700

    fetching user's posts done

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -31,7 +31,22 @@ const getAllPosts = async (req, res) => {
   }
 };
 
+const getUserPosts = async (req, res) => {
+  try {
+    const result = await post.find({user: req.params.userEmail});
+    res.json({
+      status: true,
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      error: error.message,
+    });
+  }
+};
 module.exports = {
     createNewPost,
-    getAllPosts
+    getAllPosts,
+    getUserPosts
 };
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -5,6 +5,7 @@ const postControllers = require("../controllers/post.controller");
 const router = express.Router();
 
 router.get("/", postControllers.getAllPosts)
+router.get("/:userEmail", postControllers.getUserPosts)
 router.post("/", postControllers.createNewPost)
 
 module.exports = router;
